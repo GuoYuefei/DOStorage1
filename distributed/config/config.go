d5b9ac17b1bb2c9eeb8a4f523e293a060260d475
Damon <guoyuefei@protonmail.com>
2020-08-31 10:58:53 +0800

    增加配置文件功能

--- distributed/config/config.go
+++ distributed/config/config.go
@@ -1,6 +1,8 @@
 package config
 
 import (
+	"gopkg.in/yaml.v2"
+	"io/ioutil"
 	"os"
 )
 
@@ -17,9 +19,21 @@ func init() {
 	ServerInf = &SInf{
 		LISTEN_ADDRESS: "localhost:23333",
 	}
+	ServerInf.SPub = Pub
 	ServerData = &SData{
 		LISTEN_ADDRESS: "localhost:23334",
 		STORAGE_ROOT: "./data",
+	}
+	ServerData.SPub = Pub
+
+	// 配置文件优先级在环境变量优先级之下
+	dataconfigfile, err := ioutil.ReadFile("./data.yml")
+	if err == nil {
+		yaml.Unmarshal(dataconfigfile, ServerData)
+	}
+	interfaceConfigFile, err := ioutil.ReadFile("./interface.yml")
+	if err == nil {
+		yaml.Unmarshal(interfaceConfigFile, ServerInf)
 	}
 
 	if os.Getenv("RABBITMQ_SERVER") != "" {
@@ -38,23 +52,20 @@ func init() {
 		ServerData.STORAGE_ROOT = os.Getenv("STORAGE_ROOT")
 	}
 
-	// todo config
-
-
 }
 
 type SPub struct {
-	ES_SERVER string
-	RABBITMQ_SERVER string
+	ES_SERVER string			`yaml:"es_server"`
+	RABBITMQ_SERVER string		`yaml:"rabbitmq_server"`
 }
 
 type SInf struct {
-	*SPub
-	LISTEN_ADDRESS string
+	*SPub						`yaml:"public"`
+	LISTEN_ADDRESS string		`yaml:"listen_address"`
 }
 
 type SData struct {
-	*SPub
-	LISTEN_ADDRESS string
-	STORAGE_ROOT string
+	*SPub						`yaml:"public"`
+	LISTEN_ADDRESS string		`yaml:"listen_address"`
+	STORAGE_ROOT string			`yaml:"storage_root"`
 }
--- go.mod
+++ go.mod
@@ -6,4 +6,5 @@ require (
 	github.com/astaxie/beego v1.12.2
 	github.com/google/uuid v1.1.1
 	github.com/streadway/amqp v1.0.0
+	gopkg.in/yaml.v2 v2.2.8
 )
