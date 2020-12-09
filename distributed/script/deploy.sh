0fbbc2f212d7949219a659792ffb67c579c22726
Solomon <GuoYuefei@protonmail.com>
2020-12-09 23:37:06 +0800

    Update deploy.sh

--- distributed/script/deploy.sh
+++ distributed/script/deploy.sh
@@ -5,7 +5,7 @@
 ## 且部署时在部署机子上重新构建，所以需要go、git环境  // 使用这种方法也是因为架构，系统文件版本不同会导致二进制文件无法运行
 
 ## 确保在用户目录下有这么几个文件夹
-ssh ubuntu@119.29.5.95 "cd ~ ; mkdir -p dos/build/var ; mkdir ~/DOStorage1"
+ssh ubuntu@119.29.5.95 "cd ~ ; mkdir -p dos/build/var ; mkdir -p ~/DOStorage1"
 
 ### 复制内容
 scp -r ./** ubuntu@119.29.5.95:~/DOStorage1/
